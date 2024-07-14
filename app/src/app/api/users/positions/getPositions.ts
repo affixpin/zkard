13a8cd78156cc1cc8b4eeb1100b94b228a74f179
Korney Vasilchenko <starkkor@gmail.com>
2024-07-14 01:51:36 +0100

    -____-

--- app/src/app/api/users/positions/getPositions.ts
+++ app/src/app/api/users/positions/getPositions.ts
@@ -1,24 +1,54 @@
 import { createPublicClient, formatUnits, getContract, http } from "viem";
 import { usersDB } from "../db";
 
-const polygon = {
+const link = {
   getProxyUSD: async (address: `0x${string}`) => {
-    const maticPrice = 0.5;
-    const providerURL = "https://polygon-rpc.com";
+    const usdcAddress = "0xf8fb3713d459d7c1018bd0a49d19b4c44290ebe5";
+
+    const abi = [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ] as const;
+
+    const providerURL = "https://ethereum-sepolia-rpc.publicnode.com";
     const client = createPublicClient({
       transport: http(providerURL),
     });
-    const balance = await client.getBalance({ address });
-    const balanceFloat = parseFloat(formatUnits(balance, 18));
-    return balanceFloat * maticPrice;
+    const contract = getContract({
+      address: usdcAddress,
+      abi,
+      client,
+    });
+
+    const balance = await contract.read.balanceOf([address]);
+
+    const balanceFloat = parseFloat(formatUnits(balance, 6));
+    return balanceFloat;
   },
-  id: "Polygon MATIC",
-  logoURL: "https://cryptologos.cc/logos/polygon-matic-logo.png",
+  id: "Sepolia LINK",
+  logoURL: "https://cryptologos.cc/logos/chainlink-link-logo.png",
 };
 
 const cirle = {
   getProxyUSD: async (address: `0x${string}`) => {
-    const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+    const usdcAddress = "0xda9d4f9b69ac6c22e444ed9af0cfc043b7a7f53f";
 
     const abi = [
       {
@@ -42,7 +72,7 @@ const cirle = {
       },
     ] as const;
 
-    const providerURL = "https://polygon-rpc.com";
+    const providerURL = "https://ethereum-sepolia-rpc.publicnode.com";
     const client = createPublicClient({
       transport: http(providerURL),
     });
@@ -57,14 +87,14 @@ const cirle = {
     const balanceFloat = parseFloat(formatUnits(balance, 6));
     return balanceFloat;
   },
-  id: "Polygon Circle USDC",
+  id: "Sepolia Circle USDC",
   logoURL:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Circle_USDC_Logo.svg/2048px-Circle_USDC_Logo.svg.png",
 };
 
-const apeCoin = {
+const wbtc = {
   getProxyUSD: async (address: `0x${string}`) => {
-    const usdcAddress = "0xB7b31a6BC18e48888545CE79e83E06003bE70930";
+    const usdcAddress = "0x29f2d40b0605204364af54ec677bd022da425d03";
 
     const abi = [
       {
@@ -88,7 +118,7 @@ const apeCoin = {
       },
     ] as const;
 
-    const providerURL = "https://polygon-rpc.com";
+    const providerURL = "https://ethereum-sepolia-rpc.publicnode.com";
     const client = createPublicClient({
       transport: http(providerURL),
     });
@@ -102,12 +132,12 @@ const apeCoin = {
     const balanceFloat = parseFloat(formatUnits(balance, 6));
     return balanceFloat;
   },
-  id: "ApeCoin",
+  id: "Sepolia WBTC",
   logoURL:
     "https://img.bitgetimg.com/multiLang/coinPriceLogo/25608447a790af93320543724174878a1701537690449.png",
 };
 
-const positionConfigs = [polygon, cirle, apeCoin];
+const positionConfigs = [link, cirle, wbtc];
 
 export async function getPositions(userId: string) {
   const db = await usersDB();
