c0d20280d28dfefaf8c44c9cef0431838a9a59c4
Korney Vasilchenko <starkkor@gmail.com>
2024-07-14 01:16:02 +0100

    Remove seed coz dima asked

--- app/src/app/api/users/seed.ts
+++ app/src/app/api/users/seed.ts
@@ -1,17 +1,3 @@
 import { User } from "./entity";
 
-export const usersSeed: User[] = [
-	{
-		id: "root",
-		balance: 0,
-		creditLimit: 0,
-		creditSpent: 0,
-		defiCollateral: 0,
-		card: {
-			number: "1111 2222 3333 4444",
-			expiry: "04/20",
-		},
-		defiCollateralEnabled: 0,
-		positions: {},
-	},
-];
+export const usersSeed: User[] = [];
